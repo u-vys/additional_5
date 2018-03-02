609abc06cab705a2729a0f84f45883b74d4b7531
u-vys <vn.vysotski@gmail.com>
2018-03-02 17:24:49 +0300

    additional_5-impl

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    var array = [];
+    var i, j, n;
+    if (str.length % 2 != 0) {
+        return false;
+    }
+    for (n = 0; n < str.length; n++) {
+        array [n] = str [n];
+    }
+    var flag = true;
+    while (flag) {
+        flag = false;
+        for (i = 0; i < array.length - 1; i++) {
+            for (j = 0; j < bracketsConfig.length; j++) {
+                if (array [i] === bracketsConfig [j] [0] && array [i + 1] === bracketsConfig [j] [1]) {
+                    array.splice(i, 2);
+                    flag = true;
+                    break;
+                }
+            }
+        }
+
+    }
+    return (array.length === 0) ? true : false;
 }
